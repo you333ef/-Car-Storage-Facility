8de8cc43a6e6d153b4a8bbc09cab47378b97646c
YOUSEF <ghhjhkkoooppppplll@gmail.com>
2025-03-05 22:46:41 +0200

    Update image import paths using alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./", // يضمن أن المسارات تعمل بشكل صحيح في Vercel
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // يسهل استدعاء الملفات داخل src
+    },
+  },
+  assetsInclude: ["**/*.svg"], // يضمن تضمين صور SVG في الـ Build
+});
