06596b7b611155916511e9206ce03efc70b99962
YOUSEF <ghhjhkkoooppppplll@gmail.com>
2025-03-12 17:29:48 +0200

    l

--- src/Components/DG.jsx
+++ src/Components/DG.jsx
@@ -3,8 +3,15 @@ import sst from '../assets/images/Car-details_compo.svg'
 import STY from './dg.module.css';
 const DG = () => {
   return (
-    <div className={`${STY.details}`}>
+    <div className="">
+        <div className="container p-2">
+        <button className={`mt-4 ${STY.back_btn}`}>back</button>
+
+        </div>
+        <div className={`${STY.details}`}>
         <img className={`${STY.det_image}`} src={sst} alt="" />
+
+        </div>
     </div>
   )
 }
