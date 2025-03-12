72f147869d06f52812e55600690ad0719ca69967
YOUSEF <ghhjhkkoooppppplll@gmail.com>
2025-03-12 17:54:23 +0200

    m

--- src/Components/DG.jsx
+++ src/Components/DG.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import sst from '../assets/images/Car-details_compo.svg'
 import STY from './dg.module.css';
+import { useNavigate } from 'react-router-dom';
+import { FaAnglesLeft } from "react-icons/fa6";
+
 const DG = () => {
+
+let navigate=useNavigate()
+let OOPO=()=>{
+    navigate('/')
+}
+
+
   return (
     <div className="">
-        <div className="container p-2">
-        <button className={`mt-4 ${STY.back_btn}`}>back</button>
+        <div className="container p-2 align-items-center ">
+        <button className={`mt-4 ${STY.back_btn}`} onClick={OOPO}><FaAnglesLeft />
+         SEE MORE</button>
 
         </div>
         <div className={`${STY.details}`}>
